85b48474a245e49b71dc9773fe1136a866c515a2
TopFidgets <razzoukhamza@charif.ma>
2026-07-14 12:49:30 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Orbital PvP Club 🌌",
+        name: "Ducky Universe 🦆",
         // Activity type number (0 = Playing).
         type: 0,
       },
